8fd3650c4a2fd4bf498ec881d1c490469bdbfbef
ArkanPrime <151643699+ArkanPrime@users.noreply.github.com>
2024-03-16 20:15:53 +0100

    Update server.js
    fix sur la route de connectDB

--- Patient/server.js
+++ Patient/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import connectDB from './src/config/database.js';
+import connectDB from './config/database.js';
 
 const app = express();
 
