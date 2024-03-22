640d411d651c5f6ac4e8654f424aa32470ac35ef
ArkanPrime <151643699+ArkanPrime@users.noreply.github.com>
2024-03-22 18:06:14 +0100

    test de app.jsx

--- Ordonnance/server.js
+++ Ordonnance/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 3010 || 5000 || 8080 || 8100;
+const PORT = process.env.PORT || 3010 ;
 
 
 
---
+++ front-end/src/App.js
@@ -0,0 +1,12 @@
+import Ordonnance from './pages/Ordonnance';
+import './App.css';
+
+function App() {
+  return (
+
+    <Ordonnance />
+    )
+
+}
+
+export default App;
