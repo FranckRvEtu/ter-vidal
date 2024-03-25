8c3d28159ff297725572bc7182335318c7977c09
Moussa Etienne <151617399+EtienneM9@users.noreply.github.com>
2024-03-25 16:57:24 +0100

    Update medecinController.js
    Changement des export en require

--- Medecin/controllers/medecinController.js
+++ Medecin/controllers/medecinController.js
@@ -3,7 +3,7 @@ import Medecin from "../models/medecinModel.js";
 
 
 // cette fonction verifie pas si le Medecin existe déjà
-export const addMedecin = async (req, res) => {
+const addMedecin = async (req, res) => {
     // on prend les infos de req.body et on les stock
     const { name, firstname, birthdate, email, tel, password} = req.body;
     try {
@@ -27,7 +27,7 @@ export const addMedecin = async (req, res) => {
 
 
 
-export const getMedecin = async (req, res) => {
+const getMedecin = async (req, res) => {
     try {
         // Recherche du Medecin par son ID (remarquez que req.params.id est utilisé ici)
         const { id } = req.params;
@@ -47,7 +47,7 @@ export const getMedecin = async (req, res) => {
 }
 
 
-export const updateMedecin = async (req, res) => {
+const updateMedecin = async (req, res) => {
     const { id } = req.params;
     const update = req.body;
 
@@ -64,7 +64,7 @@ export const updateMedecin = async (req, res) => {
 };
 
 
-export const deleteMedecin = async (req, res) => {
+const deleteMedecin = async (req, res) => {
     const { id } = req.params;
 
     try {
@@ -77,4 +77,12 @@ export const deleteMedecin = async (req, res) => {
         console.error(error);
         res.status(500).json({ message: "Erreur lors de la suppression du Medecin" });
     }
+};
+
+module.exports = {
+    getMedecin,
+    addMedecin,
+    deleteMedecin,
+    updateMedecin,
+    // Ajoutez les a
 };
