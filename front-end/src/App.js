7fecd0f1a8578cc26d6da57866a5f60b13d52dfa
ArkanPrime <ramy.massal@etu.umontpellier.fr>
2024-03-26 19:26:03 +0100

    sidebar material ongoing

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,10 +3,11 @@ import Login from './pages/Login';
 import './App.css';
 import Dashboard from './Components/Dashboard';
 import Header from './Components/Header';
+import Drawer from './Components/Drawer';
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Drawer />
       
     </div>
   );
