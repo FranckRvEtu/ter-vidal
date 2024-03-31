af732b6dd56d4690fe425968ee262027dc4ba4b0
Etiennem21 <etiennemoussa55@gmail.com>
2024-03-31 16:41:11 +0200

    la bubulle des rdvs

--- front-end/src/pages/Calendar.js
+++ front-end/src/pages/Calendar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useRef, useEffect } from 'react';
+import { Tooltip } from 'bootstrap'; // Assurez-vous d'avoir installé bootstrap
 import Fullcalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
@@ -10,6 +11,28 @@ import '@fullcalendar/core/locales-all';
 export default function Calendar({ listRDV }){
 
   console.log("Dans la page", listRDV);  
+  let tooltip = null;
+
+  const handleMouseEnter = (info) => {
+      const calendarBoundingRect = document.querySelector('.calendar').getBoundingClientRect();
+      const eventBoundingRect = info.el.getBoundingClientRect();
+
+      const placement = eventBoundingRect.top < calendarBoundingRect.top + 200 ? 'left' : 'top';
+
+      tooltip = new Tooltip(info.el, {
+      title: 'Lieu: ' + info.event.extendedProps.department + ' - ' + info.event.description,
+      placement: placement,
+      trigger: 'hover',
+      container: 'body'
+    });
+  };
+
+  const handleMouseLeave = () => {
+    if (tooltip) {
+      tooltip.dispose();
+      tooltip = null;
+    }
+  };
 
   return (
     <div className="calendar">
@@ -27,7 +50,31 @@ export default function Calendar({ listRDV }){
         height={"auto"}
         locale='fr' // Langue et timezone française
         allDaySlot={false} // Enlève la ligne "all day"
-        events = {listRDV}
+        events = {[
+          {
+            title: 'RDV Francky',
+            start: '2024-03-28T10:30:00',
+            end: '2024-03-28T11:30:00',
+            extendedProps: {
+              department: 'Lozère',
+              description: 'Description de l"événement'
+            }
+          },
+            
+        
+          {
+            title: 'RDV Etienne',
+            start: '2024-03-27T08:30:00',
+            end: '2024-03-27T10:30:00',
+            extendedProps: {
+              department: 'Lozère',
+            },
+            description: 'Description de l"événement'
+          }
+          // more events ...
+        ]}
+        eventMouseEnter={handleMouseEnter} 
+        eventMouseLeave={handleMouseLeave}
 
       />
     </div>
