08049dc32e3118e94e10d624536762ae877b640c
ArkanPrime <ramy.massal@etu.umontpellier.fr>
2024-04-05 19:52:00 +0200

    un peu de netoyage

--- Visite/routes/visiteRoute.js
+++ Visite/routes/visiteRoute.js
@@ -1,10 +1,17 @@
-const express = require('express');
-const {addVisite, deleteVisite,getVisite,getAllVisites}  = require("../controllers/visiteController");
+const express = require("express");
+const {
+  addVisite,
+  deleteVisite,
+  getVisite,
+  getAllVisites,
+  daylyVisiteCount,
+} = require("../controllers/visiteController");
 const router = express.Router();
 
-router.post('/addVisite',addVisite);
-router.get('/deleteVisite/:id',deleteVisite);
-router.get('/getVisite/:id',getVisite);
-router.get('/allVisites',getAllVisites);
+router.post("/addVisite", addVisite);
+router.get("/deleteVisite/:id", deleteVisite);
+router.get("/getVisite/:id", getVisite);
+router.get("/allVisites", getAllVisites);
+router.get("/daylyVisiteCount", daylyVisiteCount);
 
 module.exports = router;
