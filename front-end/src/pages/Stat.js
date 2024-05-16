eae23c545128a2f390cde209695fff49c5b1caad
ArkanPrime <ramy.massal@etu.umontpellier.fr>
2024-05-16 17:17:13 +0200

    timestamp pour prescription

--- front-end/src/pages/Stat.js
+++ front-end/src/pages/Stat.js
@@ -1,20 +1,28 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import { Typography, Card, Paper, CardHeader, Box } from "@mui/material";
-import { styled, useTheme } from "@mui/material/styles";
+import {
+  Typography,
+  Card,
+  Paper,
+  CardHeader,
+  Box,
+  FormControl,
+  Select,
+  MenuItem,
+  InputLabel,
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
 import Chart from "react-apexcharts";
 import chroma from "chroma-js";
 
 function generateColors(baseColor, pieces) {
   const baseHue = chroma(baseColor).get("hsl.h");
   let colors = [];
-
   for (let i = 0; i < pieces; i++) {
-    const hue = (baseHue + (240 / pieces) * i) % 360; // ensure hue stays within 0-360
+    const hue = (baseHue + (360 / pieces) * i) % 360;
     const color = chroma(baseColor).set("hsl.h", hue).css();
     colors.push(color);
   }
-
   return colors;
 }
 
@@ -34,63 +42,92 @@ const StyledChart = styled(Chart)(({ theme }) => ({
 }));
 
 function MedicationUsageChart({ title, subheader }) {
-  const theme = useTheme();
   const [fullData, setFullData] = useState([]);
   const [chartData, setChartData] = useState(null);
+  const [selectedMeds, setSelectedMeds] = useState([]);
+  const [timelineData, setTimelineData] = useState([]);
 
   useEffect(() => {
     const fetchStats = async () => {
-      try {
-        const response = await fetch("http://localhost:3013/allPrescriptions");
-        const data = await response.json();
-        setFullData(data);
+      const response = await fetch("http://localhost:3013/allPrescriptions");
+      const jsonData = await response.json();
+      setFullData(jsonData);
 
-        const medicationCounts = {};
-        data.forEach((prescription) => {
-          const medication = prescription.Medicament;
-          medicationCounts[medication] =
-            (medicationCounts[medication] || 0) + 1;
-        });
+      const medicationCounts = {};
+      jsonData.forEach((prescription) => {
+        const medication = prescription.Medicament;
+        medicationCounts[medication] = (medicationCounts[medication] || 0) + 1;
+      });
 
-        const sortedMedications = Object.entries(medicationCounts).sort(
-          (a, b) => b[1] - a[1]
-        );
-        const topMedications = sortedMedications.slice(0, 8);
-        const otherCount = sortedMedications
-          .slice(8)
-          .reduce((acc, curr) => acc + curr[1], 0);
+      const sortedMedications = Object.entries(medicationCounts).sort(
+        (a, b) => b[1] - a[1]
+      );
+      const topMedications = sortedMedications.slice(0, 8);
+      const otherCount = sortedMedications
+        .slice(8)
+        .reduce((acc, curr) => acc + curr[1], 0);
 
-        const labels = topMedications.map((item) => item[0]);
-        const series = topMedications.map((item) => item[1]);
-        if (otherCount > 0) {
-          labels.push("Other");
-          series.push(otherCount);
-        }
-
-        const baseColor = "#D62F45"; // Example base color
-        const colors = generateColors(baseColor, labels.length);
-
-        setChartData({ labels, series, colors });
-      } catch (error) {
-        console.error("Failed to fetch statistics", error);
+      const labels = topMedications.map((item) => item[0]);
+      const series = topMedications.map((item) => item[1]);
+      if (otherCount > 0) {
+        labels.push("Other");
+        series.push(otherCount);
       }
+
+      const baseColor = "#D62F45";
+      const colors = generateColors(baseColor, labels.length);
+      setChartData({ labels, series, colors });
     };
 
     fetchStats();
   }, []);
 
-  // Handle cases where chartData may not be fully defined
+  useEffect(() => {
+    const endDate = new Date();
+    const startDate = new Date(
+      endDate.getFullYear(),
+      endDate.getMonth(),
+      endDate.getDate() - 7
+    );
+
+    const filteredData = fullData.filter(
+      (item) =>
+        new Date(item.Date) >= startDate && new Date(item.Date) <= endDate
+    );
+
+    console.log("Filtered Data:", filteredData); // Check what data is being filtered
+
+    const series = selectedMeds.map((med) => ({
+      name: med,
+      data: filteredData
+        .filter((item) => item.Medicament === med)
+        .map((d) => ({
+          x: new Date(d.Date).getTime(),
+          y: 1,
+        })),
+    }));
+
+    console.log("Series Data:", series); // Verify the structure and contents of the series data
+
+    setTimelineData(series);
+  }, [selectedMeds, fullData]);
+
+  const handleMedicationChange = (event) => {
+    setSelectedMeds(event.target.value);
+  };
+
   if (!chartData) {
-    return <Typography>Loading chart data...</Typography>; // or any other fallback UI
+    return <Typography>Loading chart data...</Typography>;
   }
-  const chartOptions = {
+
+  const pieChartOptions = {
     chart: {
       type: "pie",
       toolbar: {
         show: true,
       },
     },
-    colors: chartData.colors, // Use dynamically generated colors
+    colors: chartData.colors,
     labels: chartData.labels,
     responsive: [
       {
@@ -118,13 +155,48 @@ function MedicationUsageChart({ title, subheader }) {
     },
   };
 
+  const lineChartOptions = {
+    chart: {
+      type: "line",
+      height: 350,
+      zoom: {
+        enabled: false,
+      },
+    },
+    stroke: {
+      curve: "smooth",
+    },
+    xaxis: {
+      type: "datetime",
+      title: {
+        text: "Date",
+      },
+      labels: {
+        formatter: function (val) {
+          return new Date(val).toLocaleDateString(); // Convertit le timestamp en date lisible
+        },
+      },
+      // Assurez-vous que l'axe X représente une période de 7 jours
+      min: new Date(new Date().setDate(new Date().getDate() - 7)).getTime(), // Définir le minimum à 7 jours avant
+      max: new Date().getTime(), // Définir le maximum à aujourd'hui
+      tickAmount: 7, // Nombre de ticks sur l'axe, correspondant à 7 jours
+    },
+    tooltip: {
+      shared: true,
+      intersect: false,
+    },
+    dataLabels: {
+      enabled: false,
+    },
+  };
+
   return (
     <Box>
       <Box display="flex" justifyContent="flex-end">
         <Paper>
           <CardHeader title={title} subheader={subheader} sx={{ mb: 5 }} />
           <StyledChart
-            options={chartOptions}
+            options={pieChartOptions}
             series={chartData.series}
             type="pie"
             width="100%"
@@ -132,12 +204,31 @@ function MedicationUsageChart({ title, subheader }) {
           />
         </Paper>
       </Box>
-      <Box component="div" sx={{ overflowY: "auto", maxHeight: 300 }}>
-        {fullData.map((item, index) => (
-          <Typography key={index}>
-            {item.Medicament}: {item.Details} on {item.Date}
-          </Typography>
-        ))}
+      <Box sx={{ mt: 5, width: "50vw" }}>
+        <FormControl fullWidth sx={{ mt: 2, mb: 2 }}>
+          <InputLabel>Select Medications</InputLabel>
+          <Select
+            multiple
+            value={selectedMeds}
+            onChange={handleMedicationChange}
+            renderValue={(selected) => selected.join(", ")}
+          >
+            {[...new Set(fullData.map((item) => item.Medicament))].map(
+              (medication, index) => (
+                <MenuItem key={index} value={medication}>
+                  {medication}
+                </MenuItem>
+              )
+            )}
+          </Select>
+        </FormControl>
+        <Chart
+          options={lineChartOptions}
+          series={timelineData}
+          type="line"
+          width="100%"
+          height={300}
+        />
       </Box>
     </Box>
   );
